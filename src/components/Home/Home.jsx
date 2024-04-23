1f40e175011bae37aab1ae2f329552c7dc093062
Ditte Lykke Hansen <dittehansen143@gmail.com>
2024-04-23 22:28:33 +0200

    refactor

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,46 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './Home.css';
-import wavingDitte from '../../assets/img/wavingDitte.svg';
 import '../Animations.css';
 import { useLocation } from 'react-router-dom';
-import wavingDitteMP4 from '../../assets/waving-ditte.mp4';
-import wavingDitteMoving from '../../assets/waving-ditte-gif.gif';
-// import SVGatorComponent from '../waving-ditte';
 import ditteWavingSVG from '../../assets/waving-ditte-long.svg';
-import DitteWavingSVG from './DitteWavingSVG2';
 
 const Home = () => {
   const location = useLocation();
   const comingFromLeft = ['/'];
-
-  // useEffect(() => {
-  //   const videos = document.querySelectorAll('video');
-
-  //   videos.forEach((video) => {
-  //     video.addEventListener('mouseover', function () {
-  //       this.play();
-  //     });
-
-  //     video.addEventListener('mouseout', function () {
-  //       this.pause();
-  //     });
-
-  //     video.addEventListener('touchstart', function () {
-  //       this.play();
-  //     });
-
-  //     video.addEventListener('touchend', function () {
-  //       this.pause();
-  //     });
-
-  //     return () => {
-  //       video.removeEventListener('mouseover', handleMouseOver);
-  //       video.removeEventListener('mouseout', handleMouseOut);
-  //       video.removeEventListener('touchstart', handleTouchStart);
-  //       video.removeEventListener('touchend', handleTouchEnd);
-  //     };
-  //   });
-  // }, []); // Empty dependency array ensures that this effect runs only once, similar to componentDidMount
 
   return (
     <div
@@ -64,12 +30,11 @@ const Home = () => {
             in a team I'm helpful, positive and ambitious.
           </p>
         </div>
-        <object className='wavingDitte' data={ditteWavingSVG} type="image/svg+xml" loop></object>
-        {/* <DitteWavingSVG /> */}
-        {/* <SVGatorComponent /> */}
-        {/* <video className="wavingDitte" src={wavingDitteMP4} loop></video> */}
-        {/* <img className="wavingDitte" src={wavingDitte} alt="Waving Ditte" /> */}
-        {/* <img className="wavingDitte" src={ditteWavingSVG} alt="Waving Ditte" /> */}
+        <object
+          className="wavingDitte"
+          data={ditteWavingSVG}
+          type="image/svg+xml"
+        ></object>
       </div>
     </div>
   );
